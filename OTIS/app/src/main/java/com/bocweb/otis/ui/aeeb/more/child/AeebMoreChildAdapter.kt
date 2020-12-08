215836106a5ff7b351c09d17ed5d1683e37f498c
shaonian <617416791@qq.com>
2020-12-08 15:50:31 +0800

    多 详情

--- OTIS/app/src/main/java/com/bocweb/otis/ui/aeeb/more/child/AeebMoreChildAdapter.kt
+++ OTIS/app/src/main/java/com/bocweb/otis/ui/aeeb/more/child/AeebMoreChildAdapter.kt
@@ -1,7 +1,11 @@
 package com.bocweb.otis.ui.aeeb.more.child
 
+import android.content.Intent
+import android.view.View
 import com.bocweb.otis.R
 import com.bocweb.otis.ui.aeeb.common.AeebInfo
+import com.bocweb.otis.ui.aeeb.more.detail.AeebMoreDetailActivity
+import com.bocweb.otis.util.setClickNoRepeat
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 
@@ -12,9 +16,14 @@ class AeebMoreChildAdapter(data: MutableList<AeebInfo>) :
     override fun convert(holder: BaseViewHolder, item: AeebInfo) {
         holder.apply {
             setBackgroundColor(R.id.iv_bg, item.image1)
-            setText(R.id.tv_title,item.title)
-            setText(R.id.tv_desc,item.desc1)
-            setBackgroundColor(R.id.iv_no,item.image2)
+            setText(R.id.tv_title, item.title)
+            setText(R.id.tv_desc, item.desc1)
+            setBackgroundColor(R.id.iv_no, item.image2)
+
+            getView<View>(R.id.action).setClickNoRepeat {
+                val intent = Intent(context, AeebMoreDetailActivity::class.java)
+                context.startActivity(intent)
+            }
         }
     }
 }
---
+++ OTIS/app/src/main/java/com/bocweb/otis/ui/aeeb/more/detail/AeebMoreDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.bocweb.otis.ui.aeeb.more.detail
+
+import com.bocweb.otis.R
+import com.bocweb.otis.app.base.BaseFragment
+
+class AeebMoreDetailActivity : BaseFragment() {
+    override fun getLayoutId() = R.layout.activity_aeeb_more_detail
+
+    override fun initView() {
+
+    }
+}
